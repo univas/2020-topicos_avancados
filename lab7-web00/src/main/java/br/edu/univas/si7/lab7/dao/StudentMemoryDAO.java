6dcd942cc23370aae049cef72bf1779255bd5203
Roberto Rocha <rrocha.roberto@gmail.com>
2020-05-25 11:29:03 -0300

    Basic Java Web with JSP and memory DAO.

---
+++ lab7-web00/src/main/java/br/edu/univas/si7/lab7/dao/StudentMemoryDAO.java
@@ -0,0 +1,42 @@
+package br.edu.univas.si7.lab7.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import br.edu.univas.si7.lab7.entities.Student;
+
+public class StudentMemoryDAO {
+
+	private Map<Integer, Student> studentList = new HashMap<>();
+
+	public void save(Student student) {
+		System.out.println("StudentMemoryDAO: save: " + student);
+		studentList.put(student.getRegister(), student);
+	}
+
+	public void update(Student student) {
+		System.out.println("StudentMemoryDAO: update: " + student);
+		studentList.put(student.getRegister(), student);
+	}
+
+	public List<Student> listAll() {
+		System.out.println("StudentMemoryDAO: listAll");
+		return new ArrayList<Student>(studentList.values());
+	}
+
+	public Student findById(int register) {
+		System.out.println("StudentMemoryDAO: findById: " + register);
+		return studentList.get(register);
+	}
+
+	public void remove(int register) {
+		System.out.println("StudentMemoryDAO: remove: " + register);
+		studentList.remove(register);
+	}
+
+	public void remove(Student toRemove) {
+		remove(toRemove.getRegister());
+	}
+}
---
+++ lab7-web00/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! At first online class...</h2>
+</body>
+</html>
